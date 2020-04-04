5e67a92c661dee3ca61abd4abce8f29af60f42ee
lukabobo <dungz1207@gmail.com>
2020-04-04 17:14:00 +0700

    wp

--- linux/shell_bash/Scripts/wp_cent7.sh
+++ linux/shell_bash/Scripts/wp_cent7.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 # cai lamp
 apache(){
-#update
-yum -y update
-yum -y upgrade
-clear
-# cai dat Apache
-yum -y install httpd
-clear
-# khoi dong apache
-systemctl start httpd
-systemctl enable httpd
+    #update
+    yum -y update
+    yum -y upgrade
+    clear
+    # cai dat Apache
+    yum -y install httpd
+    clear
+    # khoi dong apache
+    systemctl start httpd
+    systemctl enable httpd
 }
 
 mariadb(){
-# cai dat mariadb
-yum -y update
-yum -y install mariadb mariadb-server
-clear
-# khoi dong mariadb
-systemctl start mariadb
-systemctl enable mariadb
+    # cai dat mariadb
+    yum -y update
+    yum -y install mariadb mariadb-server
+    clear
+    # khoi dong mariadb
+    systemctl start mariadb
+    systemctl enable mariadb
 }
 
 php(){
-# cai dat repo php
-yum install -y epel-release yum-utils
-yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-clear
-# cai dat php
-yum-config-manager --enable remi-php74
-yum install -y php php-common php-opcache php-mcrypt php-cli php-gd php-curl php-mysqlnd
-clear
-#khoi dong lai apache
-systemctl restart httpd
+    # cai dat repo php
+    yum install -y epel-release yum-utils
+    yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+    clear
+    # cai dat php
+    yum-config-manager --enable remi-php74
+    yum install -y php php-common php-opcache php-mcrypt php-cli php-gd php-curl php-mysqlnd
+    clear
+    #khoi dong lai apache
+    systemctl restart httpd
 }
 
 
@@ -66,41 +66,41 @@ EOF
 }
 
 tai_wp(){
-#cai goi ho tro php-gd
-yum -y install php-gd php-mysql
-clear
-#download wordpress
-yum -y install wget
-wget https://wordpress.org/latest.tar.gz
-#unzip wordpress
-cd /tmp
-tar -zxvf latest.tar.gz
-clear
+    #cai goi ho tro php-gd
+    yum -y install php-gd php-mysql
+    clear
+    #download wordpress
+    yum -y install wget
+    wget https://wordpress.org/latest.tar.gz
+    #unzip wordpress
+    cd /tmp
+    tar -zxvf latest.tar.gz
+    clear
 }
 
 cau_hinh_wp(){
-# copy cac file trong thu muc wordpress toi duong dan /var/www/html
-cp -Rvf /tmp/wordpress/* /var/www/html
-#den thu muc /var/www/html/
-cd /var/www/html
-#copy file wp-config.php
-cp wp-config-sample.php wp-config.php
-#sua file wp-config.php voi cac thong tin da nhap
-sed -i -e "s/database_name_here/$mariadb/g" wp-config.php
-sed -i -e "s/username_here/"$mariauser"/g" wp-config.php
-sed -i -e "s/password_here/"$mariapass"/g" wp-config.php
-# phan quyen
-chown -R apache:apache /var/www/html/*
-chmod -R 755 /var/www/html/*
+    # copy cac file trong thu muc wordpress toi duong dan /var/www/html
+    cp -Rvf /tmp/wordpress/* /var/www/html
+    #den thu muc /var/www/html/
+    cd /var/www/html
+    #copy file wp-config.php
+    cp wp-config-sample.php wp-config.php
+    #sua file wp-config.php voi cac thong tin da nhap
+    sed -i -e "s/database_name_here/$mariadb/g" wp-config.php
+    sed -i -e "s/username_here/"$mariauser"/g" wp-config.php
+    sed -i -e "s/password_here/"$mariapass"/g" wp-config.php
+    # phan quyen
+    chown -R apache:apache /var/www/html/*
+    chmod -R 755 /var/www/html/*
 }
 
 mo_port(){
-# mo port cho dich vu web
-systemctl retart firewalld
-firewall-cmd --permanent --add-port=80/tcp      # mo port http
-firewall-cmd --permanent --add-port=443/tcp     # mo port https
-firewall-cmd --reload
-clear
+    # mo port cho dich vu web
+    systemctl retart firewalld
+    firewall-cmd --permanent --add-port=80/tcp      # mo port http
+    firewall-cmd --permanent --add-port=443/tcp     # mo port https
+    firewall-cmd --reload
+    clear
 }
 
 clear
